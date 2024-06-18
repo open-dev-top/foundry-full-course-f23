469f8f3c5cd40cad9cf2d2944c44e8a287268560
open dev <opendev.top@gmail.com>
2024-06-18 18:00:30 +0800

    Update SimpleStorage.sol

--- Lesson2/remix-simple-storage/SimpleStorage.sol
+++ Lesson2/remix-simple-storage/SimpleStorage.sol
@@ -1,2 +1,29 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.18 <0.9.0;
+pragma solidity 0.8.19;
+
+contract SimpleStorage {
+    // Basic Types
+    // boolean
+    bool hasFavoriteNumber = false;
+
+    // uint
+    // The favoriteNumber gets initialized to 0 if no value is given.
+    uint256 favoriteNumber; // 0
+
+    // int
+    int internal leastFavoriteNumber = -42;
+
+    // string
+    string favoriteNumberInText = "42";
+
+    // address
+    address myAddress = 0xF805636A05Bb396faF9Bb6954F4112d8c8F6B104;
+
+    // bytes
+    bytes32 favoriteBytes32 = "cat";
+
+
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
+}
