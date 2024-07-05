7fc35e04bea2894d8152751b401aeedd3c10273d
open dev <opendev.top@gmail.com>
2024-07-05 14:51:59 +0800

    Refactoring s_priceFeed

--- Lesson7/foundry-fund-me/test/FundMeTest.t.sol
+++ Lesson7/foundry-fund-me/test/FundMeTest.t.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.19;
 // import {Test} from "../lib/forge-std/src/Test.sol";
 import {Test, console} from "forge-std/Test.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
 
 contract FundMeTest is Test {
     // uint256 number = 1;
@@ -20,7 +21,11 @@ contract FundMeTest is Test {
 
     function setUp() external {
         // us → fundMeTest → FundMe
-        fundMe = new FundMe();
+        // fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+
+        // us → deployFundMe → FundMe
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
     }
 
     function testMinimumDollarIsFive() public {
@@ -30,9 +35,9 @@ contract FundMeTest is Test {
     function testOwnerIsMsgSender() public {
         // console.log(fundMe.i_owner());
         // console.log(msg.sender);
-        // assertEq(fundMe.i_owner(), msg.sender);
+        assertEq(fundMe.i_owner(), msg.sender);
 
-        assertEq(fundMe.i_owner(), address(this));
+        // assertEq(fundMe.i_owner(), address(this));
     }
 
     // We're going to talk about four different types of tests:
