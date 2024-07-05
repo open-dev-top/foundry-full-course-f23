0ef629954ccd473758361f5ed1c723082be6add2
open dev <opendev.top@gmail.com>
2024-07-05 14:01:04 +0800

    Update DeployFundMe.s.sol

--- Lesson7/foundry-fund-me/test/FundMeTest.t.sol
+++ Lesson7/foundry-fund-me/test/FundMeTest.t.sol
@@ -2,16 +2,36 @@
 pragma solidity 0.8.19;
 
 // import {Test} from "../lib/forge-std/src/Test.sol";
-import {Test} from "forge-std/Test.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
 
 contract FundMeTest is Test {
-    uint256 number = 1;
+    // uint256 number = 1;
+    // function setUp() external {
+    //     number = 2;
+    // }
+    // function testDemo() public view {
+    //     console.log(number);
+    //     console.log("Hello");
+    //     assertEq(number, 2);
+    // }
+
+    FundMe fundMe;
 
     function setUp() external {
-        number = 2;
+        // us → fundMeTest → FundMe
+        fundMe = new FundMe();
     }
 
-    function testDemo() public view {
-        assertEq(number, 2);
+    function testMinimumDollarIsFive() public {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public {
+        // console.log(fundMe.i_owner());
+        // console.log(msg.sender);
+        // assertEq(fundMe.i_owner(), msg.sender);
+
+        assertEq(fundMe.i_owner(), address(this));
     }
 }
